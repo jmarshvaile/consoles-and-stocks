bf1f8daedba118663182c2fe078c063dea3a34ff
Jon-Marshall Vaile <jmarshvaile@gmail.com>
2020-01-14 21:30:29 -0600

    enhance trend chart

--- analyzer/server.R
+++ analyzer/server.R
@@ -11,21 +11,52 @@ function(input, output) {
             filter = 'top'
         )
     )
-    output$test_plot <-renderPlot(
-        {
-        stocks_trend <- stocks %>% filter(ticker == input$ticker)
+    output$trend <-renderPlot({
+        stock_trend <- stocks %>% filter(ticker == input$ticker)
+        market_trend <- stocks %>% filter(ticker == input$market)
         consoles_point <- consoles
         if (input$manufacturer != "All") {
             consoles_point <- consoles_point %>% filter(manufacturer == input$manufacturer)
         }
-        consoles_point <- merge(consoles_point, stocks_trend, all.x=TRUE)
+        consoles_point <- merge(consoles_point, stock_trend, all.x=TRUE)
         consoles_point[is.na(consoles_point)] <- 0
-        
-        ggplot() +
-            geom_line(data=stocks_trend, aes(x=year, y=avg_close)) +
-            geom_point(data=consoles_point, aes(x=year, y=avg_close)) +
-            expand_limits(y=0) +
-            xlim(min(stocks_trend$year),max(stocks_trend$year))
+        if (input$market != "None"){
+            ggplot() +
+                geom_line(data=stock_trend, aes(x=year, y=avg_close), color="#56B4E9", size=3) +
+                geom_line(data=market_trend, aes(x=year, y=avg_close), color="gray", size=2) +
+                scale_y_continuous(trans='log10') +
+                geom_point(data=consoles_point, aes(x=year, y=avg_close), size=3) +
+                scale_x_continuous(breaks=stock_trend$year, 
+                                   labels=substr(stock_trend$year,3,4),
+                                   limits=c(min(stock_trend$year),max(stock_trend$year))) +
+                expand_limits(y=0) +
+                ggtitle(paste(input$ticker, "Stock Trend")) +
+                xlab("Year") +
+                ylab("\nAverage Close Price\n") +
+                theme(plot.title = element_text(size = 36, hjust = 0.5),
+                      axis.text.x = element_text(size = 16),
+                      axis.text.y = element_text(size = 16),
+                      axis.title.x = element_text(size = 22),
+                      axis.title.y = element_text(size = 22),
+                      panel.grid.minor.x = element_blank())
+        } else {
+            ggplot() +
+                geom_line(data=stock_trend, aes(x=year, y=avg_close), color="#56B4E9", size=2) +
+                geom_point(data=consoles_point, aes(x=year, y=avg_close), size=3) +
+                scale_x_continuous(breaks=stock_trend$year, 
+                                   labels=substr(stock_trend$year,3,4),
+                                   limits=c(min(stock_trend$year),max(stock_trend$year))) +
+                expand_limits(y=0) +
+                ggtitle(paste(input$ticker, "and Console Releases")) +
+                xlab("Year") +
+                ylab("\nAverage Close Price\n") +
+                theme(plot.title = element_text(size = 36, hjust = 0.5),
+                      axis.text.x = element_text(size = 16),
+                      axis.text.y = element_text(size = 16),
+                      axis.title.x = element_text(size = 22),
+                      axis.title.y = element_text(size = 22),
+                      panel.grid.minor.x = element_blank())
         }
-    )
+
+    })
 }
--- analyzer/ui.R
+++ analyzer/ui.R
@@ -4,7 +4,7 @@ header <- dashboardHeader(
 
 sidebar <- dashboardSidebar(
     sidebarMenu(
-        menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+        menuItem("Research", tabName = "research", icon = icon("comment")),
         menuItem("Data Tables", tabName = "tables", icon = icon("table"),
                  menuItem("Console Data", tabName = "console-table"),
                  menuItem("Stock Data", tabName = "stock-table")),
@@ -15,19 +15,47 @@ sidebar <- dashboardSidebar(
 
 body <- dashboardBody(
     tabItems(
-        tabItem(tabName = "dashboard",
-                h2("Dashboard"),
+        tabItem(tabName = "research",
+                h2("Video Game Consoles and the Stock Market"),
+                h3("Purpose"),
+                p("To aid in speculative stock picks that have exposure to the video game console release cycle, such as GameStop, Target, Walmart, or BestBuy."),
+                h3("Stocks"),
+                p("A stock is..."),
+                h3("Console Release Cycle"),
+                p("The console release cycle is ..."),
+                h3(""),
+                p(""),
+                h3(""),
+                p(""),
+                h3(""),
+                p(""),
+                h3("Trends"),
                 selectInput(
                     "ticker",
-                    "Stock Ticker:",
-                    unique(as.character(stocks$ticker))
+                    h5("Stock Ticker:"),
+                    c("GameStop (reseller)" = "GME",
+                      "Best Buy (reseller)" = "BBY",
+                      "Target (reseller)" = "TGT",
+                      "Walmart (reseller)" = "WMT",
+                      "Microsoft (manufacturer)" = "MSFT",
+                      "Sony (manufacturer)" = "SNEJF",
+                      "Nintendo (manufacturer)" = "NTDOF")
+                ),
+                selectInput(
+                    "market",
+                    h5("Transform and Compare (will transform axis using log10 and compare to the selected market index):"),
+                    c("None",
+                      "SP500" = "^GSPC",
+                      "DOW" = "^DJI",
+                      "NASDAQ" = "^IXIC"
+                    )
                 ),
                 selectInput(
                     "manufacturer",
-                    "Consoles by Manufacturer:",
+                    h5("Consoles by Manufacturer:"),
                     c("All", unique(as.character(consoles$manufacturer)))
                 ),
-                box(plotOutput("test_plot"))
+                plotOutput("trend", width = "100%", height = "400px")
         ),
         tabItem(tabName = "console-table",
                 h2("Console Data"),
